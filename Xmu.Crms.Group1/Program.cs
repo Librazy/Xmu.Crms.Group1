07ffebf878ef773823e8014a542110be5f434324
Librazy <im@librazy.org>
2017-12-21 14:11:21 +0800

    update example

--- Xmu.Crms.Group1/Program.cs
+++ Xmu.Crms.Group1/Program.cs
@@ -10,16 +10,6 @@ namespace Xmu.Crms.Group1
     {
         public static void Main(string[] args)
         {
-            //添加自身的RESTful API
-            //将RESTful API放在这个项目（如 ClassController.cs）
-            Startup.ControllerAssembly.Add(Assembly.GetEntryAssembly());
-
-            //添加所需要的界面
-            Startup.ControllerAssembly.Add(Assembly.GetAssembly(typeof(Xmu.Crms.Web.Group1.Program)));
-
-            //添加所需要的Service
-            Startup.ConfigureCrmsServices += collection => collection.AddGroup1UserService();
-
             var host = BuildWebHost(args);
             host.Run();
         }
@@ -29,6 +19,7 @@ namespace Xmu.Crms.Group1
             WebHost.CreateDefaultBuilder(args)
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .ConfigureServices(services => services.AddGroup1UserService().AddCrmsView("Web.Group1"))
                 .Build();
     }
 }
